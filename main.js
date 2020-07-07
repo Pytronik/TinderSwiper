b0a3d8bdbb82b2976d18acb02c1963055ab4f9ae
Pytronik <loewoffizial@gmail.com>
2020-07-07 21:33:13 +0200

    Upgrades people. We need upgrades

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-alert(document.getElementById("switchtinder"))
+chrome.storage.local.get(['key'], function(result) {
+    document.getElementById("switchtinder").checked=  result.key
+  });
+
+document.getElementById("switchtinder").onchange = function() {
+   
+    chrome.storage.local.set({key: document.getElementById("switchtinder").checked}, function() {   
+      });
+}
+
+
+if (document.getElementById("switchtinder").checked == true) {
+    //do something
+}
